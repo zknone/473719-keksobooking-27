d8a6f85f1c9765326d58cd3ef27b98d5d34f1a69
zknone <105580017+zknone@users.noreply.github.com>
2022-10-29 13:06:43 +0200

    Отрисуй меня полностью (часть 2) — для сложных проектов (#8)

--- js/render-cards.js
+++ js/render-cards.js
@@ -19,24 +19,28 @@ const renderFeatures = function (cardElement, features) {
   const featuresItems = cardElement.querySelectorAll('.popup__feature');
   const modifiers = features.map((feature) => `popup__feature--${feature}`);
   featuresItems.forEach((featureItem) => {
-    const modifier = featureItem.classList[1];
+    const modifier = featureItem.classList[1];//сравнивает со вторым классом
     if (!modifiers.includes(modifier)) {
       featureItem.remove();
     }
   });
 };
 
-dataBase.forEach((element) => {
+const createCard = function (datas) {
   const cardElement = cardTemplate.cloneNode(true);
-  cardElement.querySelector('.popup__title').textContent = element.offer.title;
-  cardElement.querySelector('.popup__text--address').textContent = element.offer.adress;
-  cardElement.querySelector('.popup__text--price').textContent = `${element.offer.price} ₽/ночь`;
-  cardElement.querySelector('.popup__type').textContent = offerTypes[element.offer.type];
-  cardElement.querySelector('.popup__text--capacity').textContent = `${element.offer.rooms} команты для ${element.offer.guests} гостей`;
-  cardElement.querySelector('.popup__text--time').textContent = `Заезд после ${element.offer.checkin}, выезд до ${element.offer.checkout}`;
-  renderFeatures(cardElement, element.offer.features);
-  cardElement.querySelector('.popup__description').textContent = element.offer.description;
-  cardElement.querySelector('.popup__photo').src = `${element.offer.photos}`;
-  cardElement.querySelector('.popup__avatar').src = `${element.author.avatar}`;
+  cardElement.querySelector('.popup__title').textContent = datas.offer.title;
+  cardElement.querySelector('.popup__text--address').textContent = datas.offer.adress;
+  cardElement.querySelector('.popup__text--price').textContent = `${datas.offer.price} ₽/ночь`;
+  cardElement.querySelector('.popup__type').textContent = offerTypes[datas.offer.type];
+  cardElement.querySelector('.popup__text--capacity').textContent = `${datas.offer.rooms} команты для ${datas.offer.guests} гостей`;
+  cardElement.querySelector('.popup__text--time').textContent = `Заезд после ${datas.offer.checkin}, выезд до ${datas.offer.checkout}`;
+  renderFeatures(cardElement, datas.offer.features);// проверка уже есть
+  cardElement.querySelector('.popup__description').textContent = datas.offer.description; //надо проверку написать
+  cardElement.querySelector('.popup__photo').src = `${datas.offer.photos}`; //проверку надо написать
+  cardElement.querySelector('.popup__avatar').src = `${datas.author.avatar}`; //проверку надо написать
   cardList.appendChild(cardElement);
-});
+};
+
+dataBase.forEach((element) => {
+  createCard(element);
+});//переделать в функцию и экспорт в мейн
